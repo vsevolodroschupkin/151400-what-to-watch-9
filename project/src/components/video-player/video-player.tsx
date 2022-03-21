dc5412c56aa2fcf8dcc711ab5b0e0da17aba7aa7
Всеволод <vsevolodros@gmail.com>
2022-03-21 15:28:51 +0500

    useeffect in film carв  WIP

--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -4,11 +4,14 @@ type VideoPlayerProps = {
   src: string;
   poster: string;
   muted: boolean;
+  width: string;
+  height: string;
+  isPlaying: boolean;
 }
 
 function VideoPlayer(props: VideoPlayerProps):JSX.Element {
-  const {src} = props;
-  const [isLoading, setIsLoading] = useState(true);
+  const {src, poster, muted, width, height, isPlaying} = props;
+  const [ , setIsLoading] = useState(true);
   const videoRef = useRef<HTMLVideoElement | null >(null);
 
   useEffect(() => {
@@ -24,7 +27,22 @@ function VideoPlayer(props: VideoPlayerProps):JSX.Element {
     };
   }, [src]);
 
-  return <video src={src} ref={videoRef}></video>;
+  useEffect(() => {
+    if (videoRef.current === null) {
+      return;
+    }
+
+    if (isPlaying) {
+      videoRef.current.play();
+      return;
+    }
+
+    videoRef.current.pause();
+    videoRef.current.load();
+
+  }, [isPlaying]);
+
+  return <video src={src} ref={videoRef} poster={poster} muted={muted} width={width} height={height}></video>;
 
 }
 
